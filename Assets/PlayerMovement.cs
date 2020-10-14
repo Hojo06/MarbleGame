2331d43d9ceea6bc973060e2ba7445f48b448bc5
Holton Johnson <holtonk.johnson@gmail.com>
2020-10-14 10:43:21 -0500

    Added Tilt Calibration and Jumping

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,27 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
 
     public bool phoneIsConnected = false;
 
+    [Header("Movement")]
+    public float jumpForce = 5;
+
+    [Header("Audio Clips")]
+    public AudioClip calibrateClip;
+    public AudioClip jumpClip;
+    public AudioClip coinClip;
+
+    [Header("Score UI")]
+    public TextMeshProUGUI scoreText;
+
     [HideInInspector]
-    public Vector3 dir, startPosition;
+    public Vector3 dir, startPosition, calibratedDir;
+
+
+
     Rigidbody rb;
+    AudioSource aud;
+    bool isGrounded = true;
+    bool canJump = false;
+
+    int score = 0;
+    int coinScore = 250;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
         startPosition = this.transform.position;
+        aud = GameObject.Find("AudioSource").GetComponent<AudioSource>();
+        CalibrateTilt();
     }
 
 
     void Update() {
         if(Input.GetKeyDown(KeyCode.R)) ResetPlayer();
         if(this.transform.position.y < -2) ResetPlayer();
+        if(!phoneIsConnected && Input.GetKeyDown(KeyCode.Space)) Jump();
     }
 
     // Update is called once per frame
@@ -29,19 +53,56 @@ public class PlayerMovement : MonoBehaviour
     {
         dir = Vector3.zero;
         if(phoneIsConnected) {
-            dir.x = Input.acceleration.x;
-            dir.z = Input.acceleration.y;
+            dir.x = Input.acceleration.x - calibratedDir.x;
+            dir.z = Input.acceleration.y - calibratedDir.z;
         } else {
             dir.x = Input.GetAxis("Horizontal");
             dir.z = Input.GetAxis("Vertical");
         } 
 
-        rb.AddForce(dir * 10);
+        rb.AddForce(dir * 7);
     }
 
     public void ResetPlayer() {
         rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
         this.transform.position = startPosition;
 
+    }
+
+    public void CalibrateTilt() {
+        calibratedDir.x = Input.acceleration.x;
+        calibratedDir.z = Input.acceleration.y;
+
+        aud.PlayOneShot(calibrateClip);
+    }
+
+    public void Jump() {
+        if(isGrounded && canJump) {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            aud.PlayOneShot(jumpClip);
+        }
+    }
+
+    void OnCollisionEnter() {
+        isGrounded = true;
+    }
+
+    void OnCollisionExit() {
+        isGrounded = false;
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Coin")) {
+            score += coinScore;
+            scoreText.text = "Score = " + score;
+            aud.PlayOneShot(coinClip);
+            Destroy(other.gameObject);
+        }
+        if(other.gameObject.name == "Jump Pickup") {
+            canJump = true;
+            aud.PlayOneShot(jumpClip);
+            Destroy(other.gameObject);
+        }
     }
 }
